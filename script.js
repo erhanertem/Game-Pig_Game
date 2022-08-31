04c59e52b1e1922377f973b72fe7f24a658f8954
Erhan Ertem <eertem@gmail.com>
2022-08-31 15:06:19 +0300

    Add newgame button event listener  and hold btn pre-filter

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ let scorePlayer1 = 0,
   currentPlayer = 1;
 
 let diceValue;
+let isGamePlayed = false;
 
 const diceFace = document.querySelector('.dice');
+const newGameBtn = document.querySelector('.btn--new');
 const rollDiceBtn = document.querySelector('.btn--roll');
+const holdRollBtn = document.querySelector('.btn--hold');
 const player1 = document.querySelector('.player--1');
 const player2 = document.querySelector('.player--2');
 const currentScores = document.querySelectorAll('.current-score');
 const playerScores = document.querySelectorAll('.score');
+const storeScore1 = document.querySelector('#score--1');
+const storeScore2 = document.querySelector('#score--2');
 const currentScore1 = document.querySelector('#current--1');
 const currentScore2 = document.querySelector('#current--2');
 
@@ -68,7 +73,29 @@ function startGame($currentPlayer) {
 }
 //INITIALIZER
 resetGame();
+//LISTEN FOR NEWGAME BUTTON
+newGameBtn.addEventListener('click', function () {
+  resetGame();
+});
 //LISTEN FOR ROLL DICE BUTTON
 rollDiceBtn.addEventListener('click', function () {
+  isGamePlayed = true;
   startGame(currentPlayer);
 });
+//LISTEN FOR HOLD BUTTON
+holdRollBtn.addEventListener('click', function () {
+  // console.log(gamePlayed, currentPlayer);
+  if (isGamePlayed) {
+    storeScore();
+    switchPlayers();
+  } else {
+    console.log('You havent initiated game yet!! Roll your dice Boi!!');
+  }
+});
+
+function storeScore() {
+  console.log(currentSessionScore);
+  currentPlayer === 1
+    ? (storeScore1.textContent = currentSessionScore)
+    : (storeScore2.textContent = currentSessionScore);
+}
