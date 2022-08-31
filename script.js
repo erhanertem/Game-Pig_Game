4b4eaa48b631fd011bcc7321c342c481f70d012d
Erhan Ertem <eertem@gmail.com>
2022-08-31 14:38:52 +0300

    Amend startgame function and added switchplayers function

--- script.js
+++ script.js
@@ -16,25 +16,13 @@ const playerScores = document.querySelectorAll('.score');
 const currentScore1 = document.querySelector('#current--1');
 const currentScore2 = document.querySelector('#current--2');
 
-//FIRST INITIALIZER
-resetGame(currentPlayer);
-
-rollDiceBtn.addEventListener('click', function () {
-  startGame(currentPlayer);
-});
-
-function resetGame($currentPlayer) {
+function resetGame() {
   for (let i = 0; i <= 1; i++) {
     currentScores[i].textContent = '0';
     playerScores[i].textContent = '0';
   } // reset both players' player and current cores
   diceFace.style.display = 'none'; // turn off dice img till next draw
-  currentPlayer = $currentPlayer;
   currentSessionScore = 0;
-
-  //switch class states
-  player1.classList.toggle('player--active');
-  player2.classList.toggle('player--active');
 }
 
 function rollDice() {
@@ -44,28 +32,43 @@ function rollDice() {
   diceFace.src = `dice-${diceValue}.png`; // show the dice face of corresponding number by diverging the src attribute of the img element
 }
 
+function switchPlayers() {
+  //switch class states
+  player1.classList.toggle('player--active');
+  player2.classList.toggle('player--active');
+}
+
 function startGame($currentPlayer) {
   rollDice(); //generate a random dice roll
 
   // control point for the dice roll
   if (diceValue === 1) {
-    // switch players
+    // switch players IN JS VAR
     if (currentPlayer === 1) {
-      currentPlayer === 2;
+      currentPlayer = 2;
     } else {
-      currentPlayer === 1;
+      currentPlayer = 1;
     }
-    //reset game
-    resetGame($currentPlayer);
-    startGame($currentPlayer);
+    //SWITCH IN HTML
+    switchPlayers();
+    // console.log(`${currentPlayer}player`);
+    // startGame(currentPlayer);
+    resetGame();
   } else {
     //add dice roll value to the current player's score
+    // console.log(currentSessionScore);
     currentSessionScore += diceValue;
-
+    // console.log(`${currentPlayer}player`);
     if (currentPlayer === 1) {
-      currentScore1.textContent = String(currentSessionScore);
+      currentScore1.textContent = `${currentSessionScore}`;
     } else if (currentPlayer === 2) {
-      currentScore2.textContent = String(currentSessionScore);
+      currentScore2.textContent = `${currentSessionScore}`;
     }
   }
 }
+//INITIALIZER
+resetGame();
+//LISTEN FOR ROLL DICE BUTTON
+rollDiceBtn.addEventListener('click', function () {
+  startGame(currentPlayer);
+});
